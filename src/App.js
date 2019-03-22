3fc4c9c2a0edda9e2200b82640b63a09fb70d464
Emrullah Ayın <e.ayin@jollytur.com>
2019-03-22 17:49:44 +0300

    added handle events and material-ui components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const styles = theme => ({
   button: {
@@ -10,18 +16,41 @@ const styles = theme => ({
   input: {
     display: 'none',
   },
+  card: {
+    maxWidth: 345,
+    margin: 'auto',
+  },
+  media: {
+    height: 140,
+  },
 });
 
 function App(props) {
-  //const { classes } = props;
-
-
+  const { classes } = props;
+  const [no, setNo] = useState(0);
+  function doClick() {
+    setNo(event => event + 2);
+  }
   return (
-    <div>
-      <Button variant="contained">
-        TC No Üret
-      </Button>
-    </div>
+    <Card className={classes.card}>
+      <Tooltip title="Tıkla-Kopyala" placement="right-start">
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {no}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Tooltip>
+      <CardActions>
+        <Button
+          variant="contained"
+          onClick={setNo === 0 ? undefined : doClick}
+        >
+          TC No Üret
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
 
