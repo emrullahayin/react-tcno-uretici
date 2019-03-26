0aa66ba37d210efcf2a37285e4171e52d5e261f1
Emrullah Ayın <e.ayin@jollytur.com>
2019-03-26 16:29:42 +0300

    imported normalize.css and typeface-roboto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import 'normalize.css';
+import 'typeface-roboto';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
